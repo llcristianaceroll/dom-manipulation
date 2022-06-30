aa9403560e7f284a6f9f79f39f1f13140e4ac22a
llcristianaceroll <llcristianaceroll@users.noreply.github.com>
2022-06-30 17:07:00 +0000

    deploy: 2e36794986f1205fe5dc659f35736270cc57d97e

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,27 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+window.fetch(url)
+.then((response) => response.json())
+.then((responseJSON) => {
+    const allItems = [];
+    responseJSON.data.forEach((element) => {
+    const imagen = document.createElement('img');
+    
+    const title = document.createElement('h2');
+    
+    const price = document.createElement('div');
+
+    const container = document.createElement('div');
+    container.append(imagen, title, price);
+
+    // document.body.appendChild(container);
+    allItems.push(container);
+
+});
+document.body.append(...allItems)
+
+});
+
