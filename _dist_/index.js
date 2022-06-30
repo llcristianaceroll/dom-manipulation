dcdcb212b128114a9cb8607a2e7d88c3559d3dbb
llcristianaceroll <llcristianaceroll@users.noreply.github.com>
2022-06-30 17:50:07 +0000

    deploy: b14bc44fcace0bd4713980e95a31deea98e0b4f5

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,18 +3,27 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
+const baseUrl = "https://platzi-avo.vercel.app"
 const url = "https://platzi-avo.vercel.app/api/avo";
 
-window.fetch(url)
+const appNode = document.querySelector('#app')
+
+window.fetch(`${baseUrl}/api/avo`)
 .then((response) => response.json())
 .then((responseJSON) => {
+    // console.log(responseJSON);
     const allItems = [];
-    responseJSON.data.forEach((element) => {
+    responseJSON.data.forEach((item) => {
+    // console.log(element.name);
+
     const imagen = document.createElement('img');
+    imagen.src = `${baseUrl}${item.image}`;
     
     const title = document.createElement('h2');
+    title.textContent = item.name;
     
     const price = document.createElement('div');
+    price.textContent = item.price;
 
     const container = document.createElement('div');
     container.append(imagen, title, price);
@@ -23,7 +32,7 @@ window.fetch(url)
     allItems.push(container);
 
 });
-document.body.append(...allItems)
+appNode.append(...allItems)
 
 });
 
