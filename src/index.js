83172ef03a518b58f53a7f6a0d2cf63a0cc3d1f9
Cristian Acero <llcristianaceroll@gmail.com>
2022-06-30 15:03:54 -0500

    styles with tailwind and international-api to prices

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@
  **/
 
 const baseUrl = "https://platzi-avo.vercel.app"
-const url = "https://platzi-avo.vercel.app/api/avo";
+// const url = "https://platzi-avo.vercel.app/api/avo";
 
 const appNode = document.querySelector('#app')
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: "currency",
+        currency: "USD",
+    }).format(price)
+    return newPrice;
+};
+ 
 
 window.fetch(`${baseUrl}/api/avo`)
 .then((response) => response.json())
@@ -16,20 +25,32 @@ window.fetch(`${baseUrl}/api/avo`)
     responseJSON.data.forEach((item) => {
     // console.log(element.name);
 
-    const imagen = document.createElement('img');
-    imagen.src = `${baseUrl}${item.image}`;
+    const image = document.createElement('img');
+    image.src = `${baseUrl}${item.image}`;
+    image.className = 'h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6'
     
     const title = document.createElement('h2');
     title.textContent = item.name;
+    // title.style = 'font-size: 2rem';
+    // title.style.fontSize = '2rem'
+    title.className = 'text-lg' // tailwind classes
     
     const price = document.createElement('div');
-    price.textContent = item.price;
+    price.textContent = formatPrice(item.price);
+    price.className = 'text-gray-600'
 
-    const container = document.createElement('div');
-    container.append(imagen, title, price);
+    const priceAndTitle = document.createElement('div')
+    priceAndTitle.className = 'text-center md:text-left'
+    priceAndTitle.appendChild(title)
+    priceAndTitle.appendChild(price)
+
+    const card = document.createElement('div');
+    card.className = 'md:flex bg-white rounded-lg p-6 hover:bg-gray-300'
+    card.appendChild(image);
+    card.appendChild(priceAndTitle);
 
     // document.body.appendChild(container);
-    allItems.push(container);
+    allItems.push(card);
 
 });
 appNode.append(...allItems)
